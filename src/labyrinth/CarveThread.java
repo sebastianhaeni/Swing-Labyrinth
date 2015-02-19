44d7a3536bc69326320d8aefd083820af1e03260
Sebastian Häni <haeni.sebastian@gmail.com>
2015-02-19 13:47:59 +0100

    making the build fail intentionally

--- src/labyrinth/CarveThread.java
+++ src/labyrinth/CarveThread.java
@@ -30,7 +30,7 @@ class CarveThread extends Thread {
 	 */
 	public CarveThread(LabyrinthModel labyrinthModel, boolean slow) {
 		_model = labyrinthModel;
-		_slow = slow;
+		_slow = slow;x
 	}
 
 	@Override
